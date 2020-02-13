9cf99ee1aeb74f08ed1642f270d94babeb26690d
Your Name <you@example.com>
2020-02-13 16:06:20 +0000

    fixing errors

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, url_for
-from application import app, db
+from application import db, app
 from application.models import Posts
 from application.forms import PostForm
 
