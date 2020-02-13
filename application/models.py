e7fbce6330ec735c82c9fc2c25a9294d2749cc02
Your Name <you@example.com>
2020-02-13 18:14:40 +0000

    Flask Login updated

--- application/models.py
+++ application/models.py
@@ -2,16 +2,7 @@ from application import db
 from application import login_manager
 from flask_login import UserMixin
 
-@login_manager.user_loader
-def load_user(id):
-    return Users.query.get(int(id))
-
 # This is the top of your Users table
-
-class USers(db.Model, UserMixin)
-
-
-
 
 class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -27,10 +18,14 @@ class Posts(db.Model):
             ])
 
 
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
         return ''.join(['UserID: ', str(self.id), '\r\n', 'Email: ', self.email])
+
+    @login_manager.user_loader
+    def load_user(id):
+        return Users.query.get(int(id))
